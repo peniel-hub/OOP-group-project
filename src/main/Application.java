1bcdf4d37abd288878db32a55dca4f78b27b70f0
Nuhamin <nuhay2828@gmail.com>
2025-12-28 09:59:11 +0300

    application

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,3 +1,8 @@
 package main;
 import javax.swing.*;
 import java.awt.*;
+import main.views.*;
+
+public class Application extends JFrame {
+    public CardLayout cardLayout = new CardLayout();
+    public JPanel container = new JPanel(cardLayout);
